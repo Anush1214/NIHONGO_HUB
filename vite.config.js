0881add53d4ef58c609822a5a082eb29ad70c77b
Anush1214 <anushcrao098@gmail.com>
2026-03-12 22:10:38 +0530

    redo

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 2000
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('firebase')) return 'vendor-firebase';
+            if (id.includes('framer-motion')) return 'vendor-framer-motion';
+            if (id.includes('react')) return 'vendor-react';
+            return 'vendor'; // all other node_modules
+          }
+        }
+      }
+    }
   }
 })
